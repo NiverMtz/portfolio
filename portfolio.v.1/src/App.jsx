ecc66180b58ebf451bfd69055edccb2d6d0007d2
NiverMtz <nivermartinezmed@gmail.com>
2021-08-27 13:18:13 -0500

    more projects

--- portfolio.v.1/src/App.jsx
+++ portfolio.v.1/src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+
+// Components
 import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Comment from "./components/Comment";
 import Project from "./components/Project";
 import Testimony from "./components/Testimony";
 
-// Testimony Images
-import pathImageTestimonyMariana from "./assets/img/mariana.jpg";
-
 // Project Images
 import projectSector from "./assets/img/sector.png";
+import projectWebFreelance from "./assets/img/website-freelance.png";
+
+// Testimony Images
+import pathImageTestimonyMariana from "./assets/img/mariana.jpg";
+import pathImageTestimonyMena from "./assets/img/ProfeMena.webp";
 
 // Projects Links
-var linkCodeSector = "https://github.com/NiverMtz/sector";
 var linkProjectSector = "https://nivermtz.github.io/sector/";
+var linkCodeSector = "https://github.com/NiverMtz/sector";
+var linkProjectWebFreelance = "https://nivermtz.github.io/freelancer-website/";
+var linkCodeWebFreelance = "https://github.com/NiverMtz/freelancer-website";
 
 // Testimonys
 var testimonyTextMariana = `"Niver Mtz es una persona con altos valores sociales, 
@@ -21,6 +27,12 @@ var testimonyTextMariana = `"Niver Mtz es una persona con altos valores sociales
                               meticuloso y se esfuerza por dar lo mejor de sí 
                               en cada proyecto que realiza."`;
 var testimonyAutorMariana = "Mariana Pérez / MAPER Consultoría";
+
+var testimonyTextMena = `"Niver Martínez es un alumno responsable, serio y con mente 
+                              abierta para buscar o aprender si esnecesario. Demuestra 
+                              su comprimiso de superación personal y desarrollo profesional 
+                              entregando en el tiempo estipulado las tareas que le son encomendadas."`;
+var testimonyAutorMena = "M.C. Alejandro Mena / FI-UNAM";
 
 function App() {
   return (
@@ -38,6 +50,16 @@ function App() {
           pathImage={pathImageTestimonyMariana}
           testimonyText={testimonyTextMariana}
           testimonyAutor={testimonyAutorMariana}
+        />
+        <Project
+          pathImage={projectWebFreelance}
+          linkProject={linkProjectWebFreelance}
+          linkCode={linkCodeWebFreelance}
+        />
+        <Testimony
+          pathImage={pathImageTestimonyMena}
+          testimonyText={testimonyTextMena}
+          testimonyAutor={testimonyAutorMena}
         />
       </div>
     </React.Fragment>
